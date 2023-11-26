1c3b7ee69d3c7ab2dae773a98c4f7829fe7696c9
yash <yasharora2678@gmail.com>
2023-11-27 01:12:15 +0530

    Updated Code with Endpoints and Validation Pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,31 +1,48 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  ParseIntPipe,
+  ValidationPipe
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
-  @Get()
-  getUsers(): [] {
-    return this.userService.getUsers();
+
+  @Get() //Get or /users/?role=intern&age=23
+  getUsers(@Query('role') role?: 'INTERN | ENGINEER | ADMIN'): {}[] {
+    return this.userService.getUsers(role);
   }
 
   @Get(':id')
-  getSpecificUser(): string {
-    return this.userService.getSpecifcUser();
+  getSpecificUser(@Param('id', ParseIntPipe) id: number): object {
+    return this.userService.getSpecifcUser(id);
   }
 
   @Post()
-  createUser(): string {
-    return this.userService.createUser();
+  createUser(@Body(ValidationPipe) user: CreateUserDto): {} {
+    return this.userService.createUser(user);
   }
 
   @Patch(':id')
-  updateUser(@Param('id') id: string): string {
-    return { id };
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) user: UpdateUserDto,
+  ): {} {
+    return this.userService.updateUser(id, user);
   }
 
   @Delete(':id')
-  deleteUser(): string {
-    return this.userService.deleteUser();
+  deleteUser(@Param('id', ParseIntPipe) id: number): {} {
+    return this.userService.deleteUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  getUsers(): [] {
-    return [];
+  private users = [
+    {
+      id: 1,
+      email: 'user1@example.com',
+      name: 'John Doe',
+      role: 'ADMIN',
+    },
+    {
+      id: 2,
+      email: 'user2@example.com',
+      name: 'Jane Doe',
+      role: 'ENGINEER',
+    },
+    {
+      id: 3,
+      email: 'user3@example.com',
+      name: 'Alice Smith',
+      role: 'INTERN',
+    },
+    {
+      id: 4,
+      email: 'user4@example.com',
+      name: 'Bob Johnson',
+      role: 'ADMIN',
+    },
+    {
+      id: 5,
+      email: 'user5@example.com',
+      name: 'Eva Williams',
+      role: 'INTERN',
+    },
+  ];
+
+  getUsers(role?: 'INTERN | ENGINEER | ADMIN'): {}[] {
+    if (role) {
+      const existingUser =  this.users.filter((item) => item.role === role);
+      if(existingUser.length === 0) throw new NotFoundException("USER ROLE NOT FOUND")
+    }
+
+    return this.users;
+  }
+  getSpecifcUser(id: number): {} {
+    const user = this.users.find((item) => item.id === id);
+    if(!user) throw new NotFoundException('User Not Found')
+    
+    return user;
   }
 
-  getSpecifcUser(): string {
-    return 'Helo from specifci user';
+  createUser(user: CreateUserDto): {} {
+    const userHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: userHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
   }
 
-  createUser(): string {
-    return 'Üser Created';
+  updateUser(
+    id: number,
+    updatedUser: UpdateUserDto
+    ){
+    this.users= this.users.map((user) => {
+      if(user.id === id){
+          return {...user, ...updatedUser}
+      }
+      return user;
+    })
+
+    return this.users;
   }
 
-  updateUser(): string {
-    return 'User Updated';
-  }
-
-  deleteUser(): string {
-    return 'User Deleted';
+  deleteUser(id: number): string {
+    return "User Deleted"
   }
 }
